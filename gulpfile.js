6f2c9cab71e09a820d47dcf9ae778528588757da
lnmunhoz <ln.munhoz@gmail.com>
2016-05-07 14:36:21 -0300

    Images optimization

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+/*
+	Uncomment to run gulp to optimze images.
+ */
+
+// var gulp = require('gulp');
+// var image = require('gulp-image');
+
+// gulp.task('image', function () {
+//   gulp.src('./public/images/**/*')
+//     .pipe(image())
+//     .pipe(gulp.dest('./public/images'));
+// });
+
+// gulp.task('default', ['image']);
