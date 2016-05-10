e095966499ed97b421541d551d6f5a06e6ef4850
shact.developer@gmail.com <shact.developer@gmail.com>
2016-05-10 12:51:25 -0300

    Limpada geral

--- client/views/technologies/technologies.js
+++ client/views/technologies/technologies.js
@@ -1,11 +1,28 @@
 Template.technologies.onRendered(function() {
   var options = {
-    items: 11,
+    items: 5,
     loop: true,
     dots: false,
     margin: 20,
     autoplay: true,
-    autoplayTimeout: 2000
+    autoplayTimeout: 2000,
+    responsive:{
+      0:{
+        items: 2,
+      },
+      768:{
+        items: 3,
+      },
+      992:{
+        items: 4,
+      },
+      1200: {
+        items: 5
+      },
+      1824: {
+        items: 6
+      }      
+    }    
   }
 
   $('.tech-carousel').owlCarousel(options);
